332966c018ed445abdf7bbc86041f1e003ff4833
Symphonic eMotions <info@symphonic-emotions.nl>
2025-09-09 21:03:59 +0200

    Init RNBOKit

---
+++ Sonik/RNBOKit/RNBOKit.h
@@ -0,0 +1,16 @@
+//
+//  RNBOKit.h
+//  RNBOKit
+//
+//  Created by Frans-Jan Wind on 09/09/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNBOKit.
+FOUNDATION_EXPORT double RNBOKitVersionNumber;
+
+//! Project version string for RNBOKit.
+FOUNDATION_EXPORT const unsigned char RNBOKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNBOKit/PublicHeader.h>
