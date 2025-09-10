e2aaec2fe16d2467fdffd709eed0491b5e017521
Symphonic eMotions <info@symphonic-emotions.nl>
2025-09-10 17:22:48 +0200

    Umbrella header RNBOKit.h

---
+++ Sonik/RNBOKit/Sources/Public/RNBOKit.h
@@ -0,0 +1,17 @@
+//
+//  RNBOKit.h
+//  Sonik
+//
+//  Created by Frans-Jan Wind on 10/09/2025.
+//
+
+
+// RNBOKit.h
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double RNBOKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char RNBOKitVersionString[];
+
+// Publieke API van het framework:
+#import "RNBOAudioUnit.h"
+#import "RNBOEventHandlerProtocol.h"
